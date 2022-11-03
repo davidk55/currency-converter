53a6deabcf46012f64e55238cfa3856d129be825
David <david.programming@protonmail.com>
2022-11-03 04:35:13 +0100

    feat: handle currency selection

--- src/components/CurrenciesListbox.tsx
+++ src/components/CurrenciesListbox.tsx
@@ -1,6 +1,95 @@
+import { useState, useEffect, Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+
+interface Props {
+  id: number;
+  selectedCurrency: string;
+  currencies: any;
+  handleSelection: (currencyName: string, id: number) => void;
+}
+interface Currency {
+  name: string;
+  symbol: string;
+  id: number;
+}
+
 function CurrenciesListbox(props: Props) {
+  const [currencies, setCurrencies] = useState([] as Currency[]);
+
+  useEffect(() => {
+    generateCurrencies();
+  }, [props.selectedCurrency, props.currencies]);
+
+  function generateCurrencies() {
+    const currs = [];
+    for (const [key, val] of Object.entries(props.currencies) as any) {
+      currs.push({ id: key, name: val.name, symbol: val.symbol } as Currency);
+    }
+    setCurrencies(currs);
+  }
+
   return (
     <div className='w-full'>
+      <Listbox
+        value={props.selectedCurrency}
+        onChange={(currencyName) =>
+          props.handleSelection(currencyName, props.id)
+        }
+      >
+        <div className='relative mt-1'>
+          <Listbox.Button
+            className={
+              'shadow-mdfocus:outline-none relative w-full cursor-default rounded-lg border border-white bg-gray-700 py-2 pl-3 pr-10 text-left focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'
+            }
+          >
+            <span className='block truncate'>{props.selectedCurrency}</span>
+            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+              <ChevronUpDownIcon
+                className='h-5 w-5 text-gray-400'
+                aria-hidden='true'
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Listbox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-gray-700 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+              {currencies.map((curr, currIdx) => (
+                <Listbox.Option
+                  key={currIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-gray-800 text-gray-300' : 'text-white'
+                    }`
+                  }
+                  value={`${curr.name} (${curr.id})`}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {`${curr.name} (${curr.id})`}
+                      </span>
+                      {selected ? (
+                        <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-amber-700'>
+                          <CheckIcon className='h-5 w-5' aria-hidden='true' />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
     </div>
   );
 }
--- src/components/CurrencyConverter.tsx
+++ src/components/CurrencyConverter.tsx
@@ -10,7 +10,18 @@ const MAX_INPUT_CHARACERS = 16;
 function CurrencyConverter() {
   const [currencies, setCurrencies] = useState<any>({});
   const [rates, setRates] = useState({});
+
+  const [selectedCurrencies, setSelectedCurrencies] = useState([
+    'Euro (EUR)',
+    'US Dollar (USD)',
+  ]);
+
   const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    applyCourse();
+  }, [selectedCurrencies, inputValue, rates, currencies]);
+
   useEffect(() => {
     async function setLastestRates() {
       const rates = await CurrencyService.getLatestRates();
@@ -31,9 +42,19 @@ function CurrencyConverter() {
 
     setInputValue(inp);
   }
+
+  function handleSelection(currencyName: string, id: number) {
+    setSelectedCurrencies((prevSelectedCurrencies) => {
+      const newSelCurrs = [...prevSelectedCurrencies];
+      newSelCurrs[id] = currencyName;
+      return newSelCurrs;
+    });
+  }
+
   return (
     <div className='flex flex-col items-center  gap-12 rounded-xl bg-gray-700 px-28 pt-12 pb-16 text-white'>
       <h1 className='text-3xl font-bold tracking-widest'>Currency Converter</h1>
+
       <input
         className='w-full rounded-lg border border-black px-3 py-1.5 text-black'
         type='text'
@@ -43,7 +64,12 @@ function CurrencyConverter() {
       />
       <div className='flex w-full flex-col items-center gap-5'>
         <CurrenciesListbox
+          selectedCurrency={selectedCurrencies[0]}
+          currencies={currencies}
+          handleSelection={handleSelection}
+          id={0}
         />
+
         <button onClick={swapSelectedCurrencies}>
           <svg
             xmlns='http://www.w3.org/2000/svg'
@@ -60,10 +86,15 @@ function CurrencyConverter() {
             />
           </svg>
         </button>
+
         <CurrenciesListbox
+          currencies={currencies}
+          selectedCurrency={selectedCurrencies[1]}
           handleSelection={handleSelection}
+          id={1}
         />
       </div>
+
       <p className='ml-10 h-10 w-full text-left font-bold'>
       </p>
     </div>
